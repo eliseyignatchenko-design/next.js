2eca745278c1a8d65322a1d21b8b635caf50b9f8
eliseyignatchenko-design <eliseyignatchenko@gmail.com>
2025-10-20 14:56:12 +0300

    first commit navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><nav>
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+      </nav>
         {children}
       </body>
     </html>
